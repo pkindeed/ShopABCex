29036008aeef967f6c1284c857f34bf31b7f1233
Povilas <taigis.kaipgis@gmail.com>
2018-09-13 00:24:36 +0300

    test

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-include_once 'includes/dbh.php';
+$dbServername = "eu-cdbr-west-02.cleardb.net";
+$dbUsername = "bbd9c1bd2b84b0";
+$dbPassword = "99af4885";
+$dbName = "heroku_70192c73b291e04";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
 
 <!DOCTYPE html>
