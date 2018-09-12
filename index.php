2358f91c592d161c29317f23d998bc27ea281d94
Povilas <taigis.kaipgis@gmail.com>
2018-09-12 22:16:51 +0300

    Update

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+include_once 'includes/dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ShopABC</title>
+	<meta charset="UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
+	<link rel="stylesheet" href="style.css">
+</head>
+<body class="homepage">
+	
+	<nav>
+		<div class="wrapper">
+	<ul>
+	<li>
+		<a href="index.php">Prekių užsakymas</a>
+		<a href="sarasas.php">Užsakymų sąrašas</a>
+	</li>
+</ul>
+</div>
+
+</nav>
+<h1 style="">Mūsų produktas - pats geriausias!</h1>
+<div class="wrapper2">
+<h2>Norėdami užsakyti produktą, įveskite savo duomenis:</h2>
+<form  method="_GET">
+	<input class="INFO" type="text" name="vardas" placeholder="Jūsų vardas">
+	<input class="INFO"type="text" name="pavardė" placeholder="Jūsų pavardė">
+	<input class="INFO"type="text" name="adresas" placeholder="Jūsų adresas">
+	
+<h2>Kai teisingai užpildysite langelius, paspauskite mygtuką ,,Pateikti".</h2>
+	<button type="submit" name="submit"	value="submit">Pateikti</button>
+</form>
+</div>
+
+<?php
+if (!empty($_GET['vardas']) && !empty($_GET['pavardė']) && !empty($_GET['adresas']) ){
+$name = $_GET['vardas'];
+$surname = $_GET['pavardė'];
+$adress = $_GET['adresas'];
+$sql = "INSERT INTO duomenys (vardas, pavarde, adresas) VALUES ('$name', '$surname', '$adress');";
+mysqli_query($conn, $sql);
+
+
+}
+?>
+
+</body>
+</html>
+
+
+
+
---
+++ sarasas.php
@@ -0,0 +1,87 @@
+<?php
+include_once 'includes/dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ShopABC</title>
+	<meta charset="UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+	<nav>
+		<div class="wrapper">
+	<ul>
+	<li>
+		<a href="index.php">Prekių užsakymas</a>
+		<a href="sarasas.php">Užsakymų sąrašas</a>
+	</li>
+</ul>
+</div>
+
+</nav>
+<div class="wrapper2">
+<h2>Norėdami ieškoti užsakymo, įveskite vardą, pavardę arba adresą į šią eilutę:</h2>
+
+
+
+<form method="POST" style="padding-bottom: 25px;">
+	<input type="text" name="search" placeholder="Ieškoti">
+	<button type="submit" name="submit-search"></button>
+</form>
+
+
+
+
+<table>
+	<tr>
+		<th>Vardas</th>
+		<th>Pavardė</th>
+		<th>Adresas</th>
+		
+
+		<?php
+
+		function contains($needle, $haystack)
+{
+    return strpos($haystack, $needle) !== false;
+}
+		
+		if (isset($_POST['search'])){
+		$search = $_POST['search'];
+		};
+	
+		$sql = "SELECT * FROM duomenys;";
+		$results = mysqli_query($conn, $sql);
+		while ($row = mysqli_fetch_assoc($results)){
+			if (empty($search)){
+			echo "<tr>";
+			echo "<td>$row[vardas]</td>";
+			echo "<td>$row[pavarde]</td>";
+			echo "<td>$row[adresas]</td>";
+			echo "</tr>";
+
+			}else{
+			if (contains($search, $row['vardas'])== true || contains($search, $row['pavarde'])== true|| contains($search, $row['adresas'])== true){
+				echo "<tr>";
+			echo "<td>$row[vardas]</td>";
+			echo "<td>$row[pavarde]</td>";
+			echo "<td>$row[adresas]</td>";
+			echo "</tr>";
+			}
+			
+		}
+		
+	};
+
+		?>
+
+
+</table>
+</div>
+
+</body>
+</html>
