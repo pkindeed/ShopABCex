63fda34192f34e1ce7937bb2e1a1c8fcf3394cfa
Povilas <taigis.kaipgis@gmail.com>
2018-09-12 23:41:18 +0300

    No php m,ain folder

--- dbh.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbServername = "eu-cdbr-west-02.cleardb.net";
-$dbUsername = "bbd9c1bd2b84b0";
-$dbPassword = "99af4885";
-$dbName = "heroku_70192c73b291e04";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-?>
