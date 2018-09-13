c28a84cc3cee4fb814bcd56e3dc67abfcb09a970
Povilas <taigis.kaipgis@gmail.com>
2018-09-13 18:03:08 +0300

    Scaling

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include_once 'includes/dbh.php';
 <head>
 	<title>ShopABC</title>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
 	<link rel="stylesheet" href="style.css">
 </head>
