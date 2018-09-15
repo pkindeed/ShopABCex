5f6da648aa9b4456c2abd3520357635be93f8458
Povilas <taigis.kaipgis@gmail.com>
2018-09-15 16:03:38 +0300

    Bandymas ivesti puslapiavima

--- sarasas.php
+++ sarasas.php
@@ -66,22 +66,47 @@ include_once 'includes/dbh.php';
 		$rikiavimas = $_POST['rikiavimas'];
 		};
 		
-		
+		//// Patikrina, kiek išvis reikia puslapių ir juos pavaizduoja
 
-		if (isset($rikiavimas) &&  $rikiavimas =='varda'){
-			$sql = "SELECT * FROM duomenys ORDER BY vardas";
-		}else if (isset($rikiavimas) &&  $rikiavimas =='pavarda'){
-			$sql = "SELECT * FROM duomenys ORDER BY pavarde";
-		}else if (isset($rikiavimas) &&  $rikiavimas =='adresa'){
-			$sql = "SELECT * FROM duomenys ORDER BY adresas";
-		}else{
-			$sql = "SELECT * FROM duomenys";
+		$totalcount = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM duomenys"));
+		$a = $totalcount/10;
+		$a = ceil($a);
+
+		for ($b=1; $b<$a; $b++){
+			  ?><a href="sarasas.php?page=<?php echo $b;?>" style="text-decoration: none"><?php 
+			  	echo $b;
+			  	 ?> </a>  
+			  <?php
 		}
 
 
+		$page = isset($_GET['page']);
+		echo $page;
+		if (empty($page) || $page == 1){
+			$page = isset($_GET['page']);
+			$page1 = 0;
+			echo $page;
+		}else{
+			$page1=($page*10)-10;
+			echo $page;
+		}
 
-		
+
+	if (isset($rikiavimas) &&  $rikiavimas =='varda'){
+			$sql = "SELECT * FROM duomenys ORDER BY vardas LIMIT $page1, 10";
+		}else if (isset($rikiavimas) &&  $rikiavimas =='pavarda'){
+			$sql = "SELECT * FROM duomenys ORDER BY pavarde LIMIT $page1, 10";
+		}else if (isset($rikiavimas) &&  $rikiavimas =='adresa'){
+			$sql = "SELECT * FROM duomenys ORDER BY adresas LIMIT $page1, 10";
+		}else{
+			$sql = "SELECT * FROM duomenys LIMIT $page1, 10";
+		}
+
+
+		//////////
+
 		$results = mysqli_query($conn, $sql);
+		
 		while ($row = mysqli_fetch_assoc($results)){
 			if (empty($search)){
 			echo "<tr>";
