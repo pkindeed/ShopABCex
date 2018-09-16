fc037b13844cc90e63ffec131d88f9c37226ac38
Povilas <taigis.kaipgis@gmail.com>
2018-09-16 22:23:21 +0300

    test

--- sarasas.php
+++ sarasas.php
@@ -162,8 +162,27 @@ if (empty($search)){
 
 ; ?> 
 <form method="_GET">
-<input  type="button" name="minus-pagenumber" value="<" onClick="window.location.href='https://shopabcfun.herokuapp.com/sarasas.php?page=<?php if(true){echo $page-1;} ?>'"></button>
-<input  type="button" name="add-pagenumber" value=">" onClick="window.location.href='https://shopabcfun.herokuapp.com/sarasas.php?page=<?php echo $page+1 ?>'"></button></form></h3>
+<input  type="button" name="minus-pagenumber" value="<" onClick="window.location.href='https://shopabcfun.herokuapp.com/sarasas.php?page=<?php 
+				
+				if($page>1){echo $page-1;}  
+				if (!empty($rikiavimas)){
+			  	echo '&rikiavimas=';
+			  	echo htmlentities($rikiavimas);
+			  }
+			  if (!empty($search)){
+			  	echo '&search=';
+			  	echo htmlentities($search);
+			  }?>'"></button>
+<input  type="button" name="add-pagenumber" value=">" onClick="window.location.href='https://shopabcfun.herokuapp.com/sarasas.php?page=<?php 
+				if($page!=$a){echo $page+1;} 
+				if (!empty($rikiavimas)){
+			  	echo '&rikiavimas=';
+			  	echo htmlentities($rikiavimas);
+			  }
+			  if (!empty($search)){
+			  	echo '&search=';
+			  	echo htmlentities($search);
+			  } ?>'"></button></form></h3>
 
 
 
