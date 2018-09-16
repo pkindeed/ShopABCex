effa05d65444d60c891c82e3843b6df7cdd1ba84
Povilas <taigis.kaipgis@gmail.com>
2018-09-16 22:17:05 +0300

    test

--- sarasas.php
+++ sarasas.php
@@ -162,7 +162,7 @@ if (empty($search)){
 
 ; ?> 
 <form method="_GET">
-<input  type="button" name="minus-pagenumber" value="<" onClick="window.location.href='https://shopabcfun.herokuapp.com/sarasas.php?page=<?php echo $page-1 ?>'"></button>
+<input  type="button" name="minus-pagenumber" value="<" onClick="window.location.href='https://shopabcfun.herokuapp.com/sarasas.php?page=<?php if(true){echo $page-1} ?>'"></button>
 <input  type="button" name="add-pagenumber" value=">" onClick="window.location.href='https://shopabcfun.herokuapp.com/sarasas.php?page=<?php echo $page+1 ?>'"></button></form></h3>
 
 
