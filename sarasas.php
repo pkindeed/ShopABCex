b53bd94adf15240b43c1b6765ef6cc6ccc5928a6
Povilas Kimutis <povilas.kimutis@gmail.com>
2019-02-05 22:39:28 +0200

    Fix

--- sarasas.php
+++ sarasas.php
@@ -20,14 +20,12 @@ include_once 'includes/dbh.php';
 		<a href="index.php">Prekių užsakymas</a>
 		&emsp;
 		<a href="sarasas.php?page=1">Užsakymų sąrašas</a>
-		<img class= "right" src="solution.png" width="90" height="90" title="Logo" alt="Logo" />
 	</li>
 </ul>
 </div>
 
 </nav>
 <div class="wrapper2">
-<h2>Norėdami ieškoti užsakymo, įveskite vardą, pavardę arba adresą į šią eilutę :</h2>
 <h2>Norėdami ieškoti užsakymo, įveskite vardą, pavardę arba adresą:</h2>
 
 
